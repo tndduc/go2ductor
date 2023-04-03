5325da4e09e56f7cccaa5fe0155f043c851b20f3
Duc TN <tnd.duc@gmail.com>
2023-04-04 00:35:04 +0700

    update appointment

--- src/main/java/com/duck/go2ductor/entity/Appointment.java
+++ src/main/java/com/duck/go2ductor/entity/Appointment.java
@@ -1,9 +1,26 @@
 package com.duck.go2ductor.entity;
 
+import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+
 /**
  * @author DucTN
  * @project duck
  * @on 4/3/2023
  */
+@Entity
+@Table(name = "appointment")
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long id_physician;
+    private Long id_patient;
+    private Timestamp start_dt_time;
+
+    private Time end_dt_time;
+    private Long id_room;
+
 }
---
+++ src/main/java/com/duck/go2ductor/entity/MedicalHistory.java
@@ -0,0 +1,9 @@
+package com.duck.go2ductor.entity;
+
+/**
+ * @author DucTN
+ * @project go2ductor
+ * @on 4/4/2023
+ */
+public class MedicalHistory {
+}
